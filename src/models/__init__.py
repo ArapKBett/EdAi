a77943af1c73cf8bb27d2685130e15d5f80cac25
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-10-02 14:20:16 +0300

    Create __init__.py

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data models and schemas
+"""
+
+from .schemas import Assignment, Course, AnalysisRequest, QuestionHelpRequest
+
+__all__ = ['Assignment', 'Course', 'AnalysisRequest', 'QuestionHelpRequest']
